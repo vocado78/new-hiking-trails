6190eef291309e1f02e2bf51e3f9aa4fc4acf90c
Ingrid Hedelin <hedeliningrid@gmail.com>
2018-12-27 18:58:17 +0100

    adds-basic-content (#2)
    * ADDS structure
    
    * ADDS package.json
    
    * ADDS webpack config
    
    * ADDS gitignore
    
    * ADDS basic content to index files and app component
    
    * UPDATES webpack config to use modular css

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.css';
+
+export default class App extends React.Component {
+    render() {
+      return (
+          <div className={styles.app}>
+            <h1>This will be my new hiking site</h1>
+          </div>
+      );
+    }
+}
