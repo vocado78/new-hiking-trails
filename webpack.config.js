7f61820298602bf677af0222c906f537994d3f55
Ingrid Hedelin <hedeliningrid@gmail.com>
2018-12-27 15:47:18 +0100

    Getting-started (#1)
    folder-file-structure

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const config = {
+  entry: './src/index.js',
+    	output: {
+        		path: path.resolve(__dirname, 'dist'),
+        		filename: 'bundle.js',
+            publicPath: '/'
+    	},
+    		module: {
+        		rules: [
+            	{
+               		test: /\.(js|jsx)$/,
+                	exclude: /node_modules/,
+               		use: 'babel-loader'
+            	},
+              {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+              },
+              {
+                test: /\.(png|svg|jpg|gif)$/,
+                 use: ['file-loader']
+              }
+        	]
+    },
+    devServer: {
+      historyApiFallback: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })]
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
+module.exports = config;
