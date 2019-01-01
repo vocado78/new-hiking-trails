127da5a659e41ab4afe4563fcddb12071e5c1955
Ingrid Hedelin <hedeliningrid@gmail.com>
2019-01-01 11:45:25 +0100

    ADDS footer component (#6)

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import content from '../../utils/content';
+import styles from './styles.css';
+
+export default function Footer() {
+  const { text } = content.footer;
+  return (
+    <footer className={styles.footer}>
+      <div>{text}</div>
+    </footer>
+  );
+}
