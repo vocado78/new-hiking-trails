b0fb00fb2c2be18c1e78040d8093016630f50dbc
vocado78 <hedeliningrid@gmail.com>
2019-01-06 11:46:55 +0100

    adds all propTypes to list and listItem

--- src/components/Results/List/List.js
+++ src/components/Results/List/List.js
@@ -72,11 +72,33 @@ export default function List(props) {
 
 List.propTypes = {
   region: PropTypes.string.isRequired,
-  // results: PropTypes.arrayOf(PropTypes.objectOf(
-  //   PropTypes.string,
-  //   PropTypes.arrayOf(PropTypes.string),
-  //   PropTypes.number
-  // )).isRequired,
+  results: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    region: PropTypes.string,
+    province: PropTypes.string,
+    start: PropTypes.string,
+    finish: PropTypes.string,
+    startLat: PropTypes.number,
+    startLon: PropTypes.number,
+    middleLat: PropTypes.number,
+    middleLon: PropTypes.number,
+    finLat: PropTypes.number,
+    finLon: PropTypes.number,
+    distance: PropTypes.string,
+    duration: PropTypes.arrayOf(PropTypes.string),
+    complete: PropTypes.string,
+    level: PropTypes.string,
+    comfort: PropTypes.arrayOf(PropTypes.string),
+    title: PropTypes.string,
+    description: PropTypes.string,
+    landscape: PropTypes.string,
+    connect: PropTypes.string,
+    stageDistances: PropTypes.string,
+    services: PropTypes.string,
+    stages: PropTypes.number,
+    access: PropTypes.string,
+    moreInfo: PropTypes.string
+  })).isRequired,
   selections: PropTypes.shape({
     selectedProvince: PropTypes.string,
     selectedComfort: PropTypes.string,
--- src/components/Results/List/ListItem/ListItem.js
+++ src/components/Results/List/ListItem/ListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import styles from './styles.css';
 
@@ -21,10 +21,32 @@ export default function ListItem(props) {
   );
 }
 
-// ListItem.propTypes = {
-//   data: PropTypes.objectOf(
-//     PropTypes.string,
-//     PropTypes.arrayOf(PropTypes.string),
-//     PropTypes.number
-//   ).isRequired
-// };
+ListItem.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string,
+    region: PropTypes.string,
+    province: PropTypes.string,
+    start: PropTypes.string,
+    finish: PropTypes.string,
+    startLat: PropTypes.number,
+    startLon: PropTypes.number,
+    middleLat: PropTypes.number,
+    middleLon: PropTypes.number,
+    finLat: PropTypes.number,
+    finLon: PropTypes.number,
+    distance: PropTypes.string,
+    duration: PropTypes.arrayOf(PropTypes.string),
+    complete: PropTypes.string,
+    level: PropTypes.string,
+    comfort: PropTypes.arrayOf(PropTypes.string),
+    title: PropTypes.string,
+    description: PropTypes.string,
+    landscape: PropTypes.string,
+    connect: PropTypes.string,
+    stageDistances: PropTypes.string,
+    services: PropTypes.string,
+    stages: PropTypes.number,
+    access: PropTypes.string,
+    moreInfo: PropTypes.string
+  }).isRequired
+};
