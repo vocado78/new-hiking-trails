a25f03eca88aa4ff1da75672dba5ad9064f7961c
vocado78 <hedeliningrid@gmail.com>
2019-01-23 18:15:08 +0100

    ADDS test get call to couchdb

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+// const env = require('../../.env');
+const nano = require('nano')(process.env.COUCHDB_URL);
 
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/welcome', (req, res) => {
-  res.send({ message: 'Welcome to my home!' })
+
+app.get('/api/test-db', (req, res) => {
+  nano.db.get('trails').then((data) => {
+    console.log('getDB response:', data);
+    res.send({ 
+      message: data
+     });
+  }).catch(err => {
+    console.log(err);
+  });
 });
+
+app.get('/api/welcome', (req, res) => {
+  res.send({ 
+    message: 'Welcome to my home!'
+   });
+});
+
 app.listen(8080, () => {
-  console.log("Server listening on port 8080")
+  console.log("Server listening on port 8080", process.env.COUCHDB_URL)
 });
