65636d0e5b37df8c3249ac3eb90470fa5dc317eb
vocado78 <hedeliningrid@gmail.com>
2019-02-03 14:00:19 +0100

    REMOVES couchdb and ADDS firebase database

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,37 @@
 const express = require('express');
-// const env = require('../../.env');
-const nano = require('nano')(process.env.COUCHDB_URL);
+const firebase = require('firebase-admin');
 
 const app = express();
+const privateKey = `-----BEGIN PRIVATE KEY-----\n${process.env.FIREBASE_KEY}\n-----END PRIVATE KEY-----\n`;
+
+firebase.initializeApp({
+  credential: firebase.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: privateKey.replace(/\\n/g, '\n')
+  }),
+  databaseURL: process.env.DATABASE_URL
+});
 
 app.use(express.static('dist'));
 
-app.get('/api/test-db', (req, res) => {
-  nano.db.get('trails').then((data) => {
-    console.log('getDB response:', data);
-    res.send({ 
-      message: data
-     });
-  }).catch(err => {
-    console.log(err);
+// app.get('/api/test-db', (req, res) => {
+//   nano.db.get('trails').then((data) => {
+//     console.log('getDB response:', data);
+//     res.send({
+//       message: data
+//     });
+//   }).catch((err) => {
+//     console.log(err);
+//   });
+// });
+
+app.get('/api/welcome', (req, res) => {
+  res.send({
+    message: 'Welcome to my home!'
   });
 });
 
-app.get('/api/welcome', (req, res) => {
-  res.send({ 
-    message: 'Welcome to my home!'
-   });
-});
-
 app.listen(8080, () => {
-  console.log("Server listening on port 8080", process.env.COUCHDB_URL)
+  console.log('Server listening on port 8080');
 });
