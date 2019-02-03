3d504d4c658560538d089bf31cc657d1da7fa6bd
Ingrid Hedelin <hedeliningrid@gmail.com>
2019-02-03 20:15:35 +0100

    use-firebase-database (#12)
    * REMOVES couchdb and ADDS firebase database
    
    * UPDATES db read ex to firebase database

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,40 @@
 const express = require('express');
-// const env = require('../../.env');
-const nano = require('nano')(process.env.COUCHDB_URL);
+const firebase = require('firebase-admin');
 
 const app = express();
+const privateKey = `-----BEGIN PRIVATE KEY-----\n${process.env.FIREBASE_KEY}\n-----END PRIVATE KEY-----\n`;
+
+firebase.initializeApp({
+  credential: firebase.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: privateKey.replace(/\\n/g, '\n')
+  }),
+  databaseURL: process.env.DATABASE_URL
+});
+
+const db = firebase.database();
 
 app.use(express.static('dist'));
 
 app.get('/api/test-db', (req, res) => {
-  nano.db.get('trails').then((data) => {
-    console.log('getDB response:', data);
-    res.send({ 
-      message: data
-     });
-  }).catch(err => {
-    console.log(err);
+  const ref = db.ref('trails/kungsleden');
+  ref.on('value', (snapshot) => {
+    console.log('getDB response:', snapshot.val());
+    res.send({
+      message: snapshot.val()
+    });
+  }, (error) => {
+    console.log('The read failed:', error.code);
   });
 });
 
 app.get('/api/welcome', (req, res) => {
-  res.send({ 
+  res.send({
     message: 'Welcome to my home!'
-   });
+  });
 });
 
 app.listen(8080, () => {
-  console.log("Server listening on port 8080", process.env.COUCHDB_URL)
+  console.log('Server listening on port 8080');
 });
